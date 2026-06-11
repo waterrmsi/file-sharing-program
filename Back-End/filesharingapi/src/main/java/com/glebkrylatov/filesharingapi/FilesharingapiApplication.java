625c1e6ce6308b744a06da378d3041584cffaba0
waterrmsi <watermsi@icloud.com>
2026-06-11 06:07:49 +0300

    added Keycloack autorization

---
+++ Back-End/filesharingapi/src/main/java/com/glebkrylatov/filesharingapi/FilesharingapiApplication.java
@@ -0,0 +1,13 @@
+package com.glebkrylatov.filesharingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesharingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesharingapiApplication.class, args);
+	}
+
+}
---
+++ Back-End/filesharingapi/src/main/java/com/glebkrylatov/filesharingapi/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.glebkrylatov.filesharingapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
+    }
+
+    //конвертер, который достаёт роли из JWT и передаёт их в апи
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("realm_access.roles");
+        converter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return jwtConverter;
+    }
+}
